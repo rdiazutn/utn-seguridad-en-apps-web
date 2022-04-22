9001fb2350ab2f94c500aba43083c43c1f0359ff
jjf-covello <covellojorge@gmail.com>
2022-04-22 20:46:50 -0300

    add mysql dockerfile and initial database script

--- backend/database/database.config.js
+++ backend/database/database.config.js
@@ -1,5 +1,7 @@
+const { query } = require('express');
 const mysql = require('mysql2');
 require('dotenv').config()
+const scripts = require('./initscript.database')
 const connection = mysql.createConnection({
   host     : process.env.DB_HOST,
   user     :  process.env.DB_USER,
@@ -7,5 +9,14 @@ const connection = mysql.createConnection({
   database :  process.env.DB_SCHEMA,
   port:  process.env.DB_PORT
 });
+
+scripts.forEach(query => connection.query(query,function (error, results, fields) {
+  if (error){
+    throw error;
+    console.log(error)
+  } 
+  return console.log(results[0]);
+}));
+
 
 module.exports = connection
---
+++ backend/database/initscript.database.js
@@ -0,0 +1,19 @@
+const scripts = [
+    'CREATE TABLE IF NOT EXISTS `test`.`user` ( `username` VARCHAR(30) NOT NULL, `password` VARCHAR(30) NOT NULL,`token` VARCHAR(50) NULL,`id` int(11) NOT NULL AUTO_INCREMENT, Primary Key(id)) ENGINE=innodb;',
+    'CREATE TABLE IF NOT EXISTS `test`.`todo` ( `desc` VARCHAR(255) NOT NULL,`id` int(11) NOT NULL AUTO_INCREMENT,`user_id` int(11) NULL, Primary Key(id), FOREIGN KEY (user_id) REFERENCES user(id) ) ENGINE=innodb;',
+    'INSERT INTO user (username, password, id) SELECT * FROM (SELECT "GiorgioGiovanni" AS username,"GG123" AS password, 1 AS id) AS tmp WHERE NOT EXISTS (SELECT username FROM user WHERE username = "GiorgioGiovanni") LIMIT 1;',
+    'INSERT INTO user (username, password,id) SELECT * FROM (SELECT "JuliusCaesar" AS username, "JC123" AS password, 2 AS id) AS tmp WHERE NOT EXISTS (SELECT username FROM user WHERE username = "JuliusCaesar") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Comprar fruta" AS `desc`, 1 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Comprar fruta") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Comprar mas fruta" AS `desc`, 1 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Comprar mas fruta") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Poner mi fruteria y abandonar la verduleria" AS `desc`, 1 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Poner mi fruteria y abandonar la verduleria") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Abandonar la fruteria" AS `desc`, 1 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Abandonar la fruteria") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Vender secretos de estado" AS `desc`, 1 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Vender secretos de estado") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Despertarse del sueño" AS `desc`, 2 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Despertarse del sueño") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Conseguir la ferrari" AS `desc`, 2 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Conseguir la ferrari") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Salvar al planeta" AS `desc`, 2 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` = "Salvar al planeta") LIMIT 1;',
+    'INSERT INTO todo (`desc`, user_id) SELECT * FROM (SELECT "Comprar secretos de estado" AS `desc`, 2 AS user_id) AS tmp WHERE NOT EXISTS (SELECT `desc` FROM todo WHERE `desc` =  "Comprar secretos de estado") LIMIT 1;'
+
+
+]
+
+module.exports = scripts
