317f0beb92d916dbc55a1b734d8c4b142220a033
Cristian <sbcristiansoria@gmail.com>
2022-05-05 20:10:58 -0300

    Crea configuración compose, ajusta dockerfiles.

--- backend/database/database.config.js
+++ backend/database/database.config.js
@@ -3,15 +3,43 @@ const mysql = require('mysql2/promise');
 require('dotenv').config()
 const scripts = require('./initscript.database')
 
+const MAX_ATTEMPTS = 10;
+const WAIT_TIME_MS = 3_000;
+
+const config = {
+  host     : process.env.DB_HOST ?? "localhost",
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_SCHEMA,
+  port     : process.env.DB_PORT,
+  multipleStatements: true,
+}
+
+const getClient = async () => {
+  let attempts = 0;
+  let connection = null;
+
+  while (attempts < MAX_ATTEMPTS) {
+    try {
+      connection = await mysql.createConnection(config);
+      break;
+    } catch (error) {
+      if (attempts < MAX_ATTEMPTS) {
+        console.log(`DB connection failed, trying again in ${WAIT_TIME_MS}ms.`);
+        await new Promise(resolve => setTimeout(resolve, WAIT_TIME_MS));
+        attempts++;
+        continue;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  return connection;
+}
+
 const createConnection = async() => {
-  const connection = await mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     :  process.env.DB_USER,
-    password :  process.env.DB_PASSWORD,
-    database :  process.env.DB_SCHEMA,
-    port:  process.env.DB_PORT,
-    multipleStatements: true
-  });
+  const connection = await getClient();
   scripts.forEach(query => connection.execute(query).then( () => {
     console.log(`Query: ${query} was sucessfully execute `)
     return "OK";
@@ -23,6 +51,5 @@ const createConnection = async() => {
 }
 
 const connection = createConnection().then( (resolvedConnection) =>  {return resolvedConnection}).catch((err) => console.log(err))
-
 
 module.exports = connection
